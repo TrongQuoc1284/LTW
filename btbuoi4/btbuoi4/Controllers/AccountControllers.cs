df79eb5fccb168ae97ef9e725ad3b46b21b188e4
TrongQuoc1284 <quoctrong1284@gmail.com>
2025-05-29 00:47:58 +0700

    Add files via upload

---
+++ btbuoi4/btbuoi4/Controllers/AccountControllers.cs
@@ -0,0 +1,27 @@
+﻿using btbuoi4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace YourNamespace.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Đăng ký thành công!";
+                return View(user);
+            }
+            
+            return View(user);
+        }
+    }
+}
---
+++ btbuoi4/btbuoi4/Models/Users.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btbuoi4.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Tên người dùng không được để trống")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Tên người dùng phải từ 3 đến 50 ký tự")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Xác nhận mật khẩu không được để trống")]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class Users : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
